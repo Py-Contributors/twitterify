3c84bace24e886d4df3057d03639cea64fff8ae7
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2022-10-23 18:45:38 +0530

    fixing config file error
    Signed-off-by: Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>

--- src/config.py
+++ src/config.py
@@ -15,11 +15,11 @@ logging.info("\n\n\nNewLog Starting...")
 def create_api():
     API_KEY = os.environ['API_KEY']
     API_SECRET_KEY = os.environ['API_SECRET_KEY']
-    ACESS_TOKEN = os.environ['ACESS_TOKEN']
-    ACESS_TOKEN_SECRET = os.environ['ACESS_TOKEN_SECRET']
+    ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+    ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
 
     auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
-    auth.set_access_token(ACESS_TOKEN, ACESS_TOKEN_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
     api = tweepy.API(auth, 
                     wait_on_rate_limit=True,
@@ -27,6 +27,7 @@ def create_api():
 
     try:
         api.verify_credentials()
+        print("Authentication OK")
     except Exception as e:
         logging.error("Error creating API", exc_info=True)
         raise e
